763ff426de914c4a0062475673e66fd8b717ebb9
Valentin THIRION <valentinthirion@gmail.com>
2015-03-09 10:56:02 +0100

    [ADD] Added a description field on the sale order lines

---
+++ lecortil_sale_order_line_description/__openerp__.py
@@ -0,0 +1,15 @@
+{
+    "name" : "Add an empty description to a sale order line",
+    "version" : "1.0",
+    "author" : "Bernard DELHEZ, AbAKUS it-solutions SARL",
+    "category": 'Sale',
+    "description": """
+    Add an empty description to a sale order line.
+    
+    This module has been developed by Bernard Delhez, intern @ AbAKUS it-solutions, under the control of Valentin Thirion.
+    """,
+    'website': "http://www.abakusitsolutions.eu",
+    'depends' : ['sale',],
+    'data': ['view/sale_order_view.xml',],
+}
+ 
---
+++ lecortil_sale_order_line_description/model/sale_order_line.py
@@ -0,0 +1,6 @@
+﻿from openerp import models, fields, api
+
+class sale_order_line_linked_products_onchange(models.Model):
+    _inherit = 'sale.order.line'
+    
+    description = fields.Char(string='Description')
