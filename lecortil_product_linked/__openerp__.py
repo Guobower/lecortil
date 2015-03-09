b918bd69a7435b3ced014a51a83b08bd4cf58bc7
Bernard Delhez <bernarddelhez@gmail.com>
2015-03-09 16:19:20 +0100

    Update __openerp__.py

--- lecortil_product_linked/__openerp__.py
+++ lecortil_product_linked/__openerp__.py
@@ -9,8 +9,7 @@ This module adds the possibility to link product and add linked to sale orders w
 This module has been developed by Bernard Delhez, intern @ AbAKUS it-solutions, under the control of Valentin Thirion.
     """,
     'website': "http://www.abakusitsolutions.eu",
-    'depends' : ['sale',],
-    'js' : ['static/src/js/add_linked_products.js',],
+    'depends' : ['sale','lecortil_product_eater',],
     'data': ['view/product_product_view.xml','view/sale_order_view.xml',],
 }
  
