ed304dfbe12b73edabdb413911de8c44a48d2684
Bernard Delhez <bernarddelhez@gmail.com>
2015-03-10 11:26:34 +0100

    Improve onchange_eater

--- lecortil_product_eater/model/sale_order_line.py
+++ lecortil_product_eater/model/sale_order_line.py
@@ -1,10 +1,11 @@
-﻿from openerp import models, fields, api
+from openerp import models, fields, api
 
-class sale_order_line_linked_products(models.Model):
+class sale_order_line_eater(models.Model):
     _inherit = 'sale.order.line'
 
     eater = fields.Many2one('cortil.eater', string="Consommateur")
     
     def onchange_eater(self, cr, uid, ids, eater, context=None):
         sol = self.pool.get('sale.order.line').browse(cr, uid, ids, context=context)
-        self.pool.get('cortil.eater').write(cr, uid, eater, {'partner': sol.order_id.partner_id.id})
+        if sol.eater and sol.eater.id:
+            self.pool.get('cortil.eater').write(cr, uid, eater, {'partner': sol.order_id.partner_id.id})
