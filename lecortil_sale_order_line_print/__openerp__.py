42b8ea24acbe0da177ab262a72101001f2017222
Bernard Delhez <bernarddelhez@gmail.com>
2015-03-17 09:41:23 +0100

    Create __openerp__.py

---
+++ lecortil_sale_order_line_print/__openerp__.py
@@ -0,0 +1,13 @@
+{
+    "name" : "Add a print button in sale order line",
+    "version" : "1.0",
+    "author" : "Bernard DELHEZ, AbAKUS it-solutions SARL",
+    "category": 'Product',
+    "description": """
+This module adds an print button in sale order line add a report for one sale oder line.
+This module has been developed by Bernard Delhez, intern @ AbAKUS it-solutions, under the control of Valentin Thirion.
+    """,
+    'website': "http://www.abakusitsolutions.eu",
+    'depends' : ['sale',],
+    'data': ['view/sale_order_view.xml','report.xml',],
+}
