26727af9e7525266fea94a80b6835f6a235d40d3
Bernard Delhez <bernarddelhez@gmail.com>
2015-03-17 09:04:56 +0100

    Create __openerp__.py

---
+++ lecortil_product_product_sol_name/__openerp__.py
@@ -0,0 +1,15 @@
+{
+    "name" : "Name on sale order lines",
+    "version" : "0.1",
+    "author" : "Bernard DELHEZ, AbAKUS it-solutions SARL",
+    "category": 'Sale',
+    "description": """
+This modules change the position of the product name and reference on the lines on sale order when a reference is set.
+
+This module has been developed by Bernard Delhez, intern @ AbAKUS it-solutions, under the control of Valentin Thirion.
+    """,
+    'website': "http://www.abakusitsolutions.eu",
+    'depends' : [
+        'sale',
+        ],
+}
