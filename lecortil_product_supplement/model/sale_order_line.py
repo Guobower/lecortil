08774273886bdf8d5cf1a3dd1cd7a09f583165fd
Valentin Thirion <valentinthirion@gmail.com>
2015-04-07 11:24:42 +0200

    Changed the accent from 'é' to 'e'

--- lecortil_product_supplement/model/sale_order_line.py
+++ lecortil_product_supplement/model/sale_order_line.py
@@ -3,5 +3,5 @@ from openerp import models, fields, api
 class sale_order_line_linked_products(models.Model):
     _inherit = 'sale.order.line'
 
-    supplement_products_ids = fields.Many2many('product.product', string="Suppléments")
+    supplement_products_ids = fields.Many2many('product.product', string="Supplements")
     product_price_unit = fields.Float(string='Product Price',default=0.0)
